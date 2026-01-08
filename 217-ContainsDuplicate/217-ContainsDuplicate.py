83ccda7c04db0832a71ed77424fce596d3e95eca
MEGH:) <meghagdsc@gmail.com>
2026-01-08 17:23:11 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,8 @@
+# Last updated: 8/1/2026, 5:23:11 pm
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
